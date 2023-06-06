2f576b282c1266320a1782fa969076790b0d1dc6
Dominic Shelton <frogamic@protonmail.com>
2023-06-07 01:34:36 +1000

    Move full stow call into flake.nix

--- flake.nix
+++ flake.nix
@@ -5,20 +5,16 @@
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
   (flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system: let
     pkgs = nixpkgs.legacyPackages."${system}";
-    env = ''
-      export NIX_PATH="nixpkgs=${nixpkgs}"
-    '';
   in {
-    devShell = pkgs.mkShell {
-      packages = [ pkgs.stow ];
-      shellHook = env;
-    };
-    apps.default = {
-      type = "app";
-      program = "${pkgs.writeShellScript "apply-wrapped" ''
-        export PATH="${pkgs.coreutils}/bin:${pkgs.stow}/bin:PATH"
-        ${pkgs.bash}/bin/bash ./apply
-      ''}";
+    packages.default = pkgs.stow;
+    apps = rec {
+      apply = {
+        type = "app";
+        program = "${pkgs.writeShellScript "apply" ''
+          ${pkgs.stow}/bin/stow --target=$HOME --no-folding --restow home
+        ''}";
+      };
+      default = apply;
     };
   }));
 }
