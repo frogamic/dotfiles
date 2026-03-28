c94e8a1e84c19521049fa03bf6073764bbe364b4
Dominic Shelton <frogamic@protonmail.com>
2026-03-28 18:33:39 +1100

    Add all dotfiles

--- home.nix
+++ home.nix
@@ -1,3 +1,31 @@
+{ lib, ... }:
+let
+	inherit (builtins) attrNames listToAttrs readDir;
+
+	genFiles = dir: listToAttrs(
+		map (
+			name: {
+				inherit name;
+				value = {
+					source = dir + "/${name}";
+					recursive = true;
+				};
+			}
+		)
+		(attrNames (readDir dir))
+	);
+in
 {
-	home.stateVersion = "24.11";
+	programs = {
+		home-manager.enable = true;
+	};
+
+	home = {
+		shellAliases = {
+			ll = "ls -alh --color=auto";
+		};
+		stateVersion = "25.11";
+
+		file = genFiles ./dotfiles;
+	};
 }
